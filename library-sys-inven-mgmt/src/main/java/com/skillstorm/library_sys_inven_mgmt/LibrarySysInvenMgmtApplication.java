3bbb4903cfa205d92e4a3cb5e1828fe4ad224b9f
Rishi <Rishi.Dhar.82@gmail.com>
2025-12-09 11:16:41 -0500

    Intialized Spring Project

---
+++ library-sys-inven-mgmt/src/main/java/com/skillstorm/library_sys_inven_mgmt/LibrarySysInvenMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.library_sys_inven_mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySysInvenMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySysInvenMgmtApplication.class, args);
+	}
+
+}
---
+++ library-sys-inven-mgmt/src/test/java/com/skillstorm/library_sys_inven_mgmt/LibrarySysInvenMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.library_sys_inven_mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySysInvenMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

6724f22b9654c1262e7eb70bef42bca1a37805cf
Rishi <Rishi.Dhar.82@gmail.com>
2025-12-10 07:58:06 -0500

    added favicon and logo in line with page header, normalized and cleaned up schema

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,13 +1,16 @@
---Lookup table
+-- Lookup table --
 CREATE TABLE biblioteca(
 	library_id SERIAL PRIMARY KEY,
 	library_name VARCHAR(255) NOT NULL,
 	library_location VARCHAR(255) NOT NULL,
-	max_capacity INT NOT NULL,
-	current_capacity INT NOT NULL
+	max_capacity INT NOT NULL
+	
+	
+	-- This is calculated based on other tables:
+	-- current_capacity INT NOT NULL
 );
 
---Lookup table
+-- Lookup table --
 CREATE TABLE book(
 	book_id SERIAL PRIMARY KEY,
 	title VARCHAR(255) NOT NULL,
@@ -15,21 +18,14 @@ CREATE TABLE book(
 	genre VARCHAR(255) NOT NULL
 );
 
---Join table
-CREATE TABLE library_book(
-	library_id INT NOT NULL REFERENCES biblioteca(library_id)
-	book_id INT NOT NULL REFERENCES book(book_id)
-	quantity INT NOT NULL
-	PRIMARY KEY(library_id, book_id)
-);
-
---global serial number, unique identifier for every single physical book
---status can be: {available, checked out, lost, overdue}
-CREATE TABLE serialNum(
-	serialNum INT SERIAL PRIMARY KEY,
+-- Join table --
+CREATE TABLE serial_number(
+	-- global serial number, unique identifier for every single physical book
+	serial_number INT SERIAL PRIMARY KEY,
 	book_id INT NOT NULL REFERENCES book(book_id),
 	library_id INT NOT NULL REFERENCES biblioteca(library_id),
-	status VARCHAR(255) NOT NULL	
+	-- book_status can be: {available, checked out, lost, overdue}
+	book_status VARCHAR(255) NOT NULL	
 );
 
 

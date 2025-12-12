2ffa8d8d3d75354be39df7e3268634329d62893f
Rishi <Rishi.Dhar.82@gmail.com>
2025-12-11 22:38:35 -0500

    added event listeners for edit and delete buttons

--- library-sys-inven-mgmt/src/main/resources/static/js/index.js
+++ library-sys-inven-mgmt/src/main/resources/static/js/index.js
@@ -4,29 +4,84 @@ let allLibraries = [];
 
 // when page is loaded/refreshed:
 document.addEventListener('DOMContentLoaded', () => {
+    
+    // get all libraryDto's and display them
     fetch(URL + '/libraries/dtos', {
         method : 'GET'      
     })
+
+    // convert JSON arary to js array of objects
     .then((returnedData) => {
         return returnedData.json();
     })
+
+    // for each object, add data to visible library table
     .then((libraries) => {
         libraries.forEach(newLibraryDto => {
             addLibraryToTable(newLibraryDto);
         });
+        return libraries;
+    })
 
-        // add a row at the bottom of the table for adding a new Library
+    /* after all libraries added to table, add another row 
+        at the bottom of the table for adding a new Library */
+    .then((libraries) => {
+        
         const addLibRow = document.createElement('tr');
         addLibRow.innerHTML = '<td></td><td></td><td></td>';
 
         // locate last row in table and add element after
         document.getElementById('trLib' + libraries.at(-1).id).after() 
     })
+
+    // add hover and click event listeners for every edit and delete button
+    .then(() => {
+        addEditEventListeners();
+        addDeleteEventListeners();
+    })
     .catch((error) => {
         // handle all 400 and 500 status code responses
         console.error(error);
     })
 });
+
+function addDeleteEventListeners() {
+    const elements = document.querySelectorAll('.delete-button');
+        elements.forEach(element => {
+            element.addEventListener('mouseenter', () =>{
+                element.style.filter = 'invert(100%) hue-rotate(120deg)';
+            })
+            element.addEventListener('mouseleave', () =>{
+                element.style.filter = 'invert(0%) hue-rotate(0deg)';
+            })
+            element.addEventListener('click', () => {
+                deleteLibraryRow(element);
+            });
+        });
+}
+
+function addEditEventListeners() {
+    const elements = document.querySelectorAll('.edit-button');
+        elements.forEach(element => {
+            element.addEventListener('mouseenter', () =>{
+                element.style.filter = 'saturate(800%)';
+            })
+            element.addEventListener('mouseleave', () =>{
+                element.style.filter = 'saturate(100%)';
+            })
+            element.addEventListener('click', () => {
+                editLibraryRow(element);
+            });
+        });
+}
+
+function editLibraryRow(editElement) {
+    console.log(editElement);
+}
+
+function deleteLibraryRow(deleteElement) {
+    console.log(deleteElement);
+}
 
 // when click on "Dashboard":
 document.getElementById('dashboard-nav').addEventListener('click',() =>{
@@ -78,8 +133,8 @@ function addLibraryToTable(newLibraryDto) {
     deleteImg.setAttribute('src','delete.png');
 
     // apply styling to edit and delete images
-    editImg.setAttribute('class','edit-delete-buttons');
-    deleteImg.setAttribute('class','edit-delete-buttons');
+    editImg.setAttribute('class','edit-button');
+    deleteImg.setAttribute('class','delete-button');
 
     // add tooltips to each image
     editImg.setAttribute('title','Edit Row');
@@ -87,7 +142,8 @@ function addLibraryToTable(newLibraryDto) {
 
     // add row id unique for library table
     tr.setAttribute('id', 'trLib' + newLibraryDto.id);
-
+    editImg.setAttribute('id', 'editLib' + newLibraryDto.id);
+    deleteImg.setAttribute('id', 'deleteLib' + newLibraryDto.id);
     // manage spacing of columns
     tr.setAttribute('class','d-flex');
     buttonColumn.setAttribute('class','col-2');
